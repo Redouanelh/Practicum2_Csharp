48b9e8e572c3ada1159ac153ba9e428922beb02d
Redouanelh <redouan_school@outlook.com>
2020-05-05 13:12:18 +0200

    Initial commit

---
+++ practicum2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace practicum2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // De random nummers setten in de 3 verschillende invoervelden. Ik set ze als String, aangezien ze in de button1_Click methode worden geparst naar een int.
+            Random random = new Random();
+
+            // De random nummers zullen telkens onder de 10 zijn.
+            num1Text.Text = random.Next(10).ToString();
+            num2Text.Text = random.Next(10).ToString();
+            num3Text.Text = random.Next(10).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(num1Text.Text);
+            int num2 = Int32.Parse(num2Text.Text);
+            int num3 = Int32.Parse(num3Text.Text);
+            
+            String output = MethodRunner.RunAllMethods(num1,num2,num3);
+            methodOutput.Text = output;
+
+            output = LambdaRunner.RunAllMethods(num1,num2,num3);
+            lambdaOutput.Text = output;
+
+            // Check of de outputs gelijk zijn
+            bool result = compareOutput();
+            
+            // Toont juiste bericht in een MessageBox
+            String outputMessage = result ? "Results OK!" : "Results not OK!";
+            System.Windows.Forms.MessageBox.Show(outputMessage);
+        }
+
+        // Methode die checkt of de outputs na het '='-teken gelijk zijn
+        private bool compareOutput()
+        {
+            // De boolean die uiteindelijk weergeeft of alle uitkomsten gelijk zijn aan elkaar
+            bool result = true;
+
+            // Alle lines van de methodOutput textfield zonder de lege lines
+            IEnumerable<String> methodOutputLines = methodOutput.Lines.Where(line => !String.IsNullOrWhiteSpace(line));
+
+            // Alle lines van de lambdaOutput textfield zonder de lege lines
+            IEnumerable<String> lambdaOutputLines = lambdaOutput.Lines.Where(line => !String.IsNullOrWhiteSpace(line));
+
+
+            for (int i=0; i<methodOutputLines.Count(); i++)
+            {
+                // Ieder methodOutput uitkomst per line
+                String methodAnswer = methodOutput.Lines[i].Split('=').Last();
+                // Ieder lambdaOutput uitkomst per line
+                String lambdaAnswer = lambdaOutput.Lines[i].Split('=').Last();
+
+                // Als de methodOutput en lambdaOutput verschillen, wordt de result boolean op false gezet
+                if (!methodAnswer.Equals(lambdaAnswer))
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ practicum2/LambdaRunner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practicum2
+{
+    class LambdaRunner
+    {
+        public static String RunAllMethods(int num1, int num2, int num3)
+        {
+            StringBuilder output = new StringBuilder();
+
+            // methode TimesThree herschreven als lambda-expressie
+            Func<int, int> timesThree = x => 3 * x;
+            output.AppendFormat("timesThree({0}) = {1}\n", num1, timesThree(num1));
+
+            // methode AddNumbers herschreven als lambda-expressie
+            Func<int, int, int, int> addNumbers = (x, y, z) => x + y + z;
+            output.AppendFormat("addNumbers({0}, {1}, {2}) = {3}\n", num1, num2, num3, addNumbers(num1, num2, num3));
+
+            // methode IsEven herschreven als lambda-expressie
+            Predicate<int> isEven = Methods.IsEven;
+            output.AppendFormat("isEven({0}) = {1}\n", num1, isEven(num1));
+
+            // methode Num2String herschreven als lambda-expressie
+            Func<int, String> num2String = x => Methods.Num2String(x);
+            output.AppendFormat("Num2String({0}) = {1}\n", num1, num2String(num1));
+
+            // methode InBetween herschreven als lambda-expressie 
+            Func<int, int, int, bool> inBetween = (x, y, z) => Methods.InBetween(x, y, z);
+            output.AppendFormat("inBetween({0}, {1}, {2}) = {3}\n", num1, num2, num3, inBetween(num1, num2, num3));
+            // (Ik wou hier eigenlijk een predicate, aangezien er een bool wordt gereturned, maar 't lukte me niet om meerdere parameters mee te geven aan een predicate)
+
+            // methode ResetName herschreven als lambda-expressie
+            Action<Person> resetName = Methods.ResetName;
+            Person p = new Person { Name = "Jan" };
+            resetName(p);
+            output.AppendFormat("resetName, daarna (Name == null) = {0}\n", p.Name == null);
+
+            return output.ToString();
+        }
+
+    }
+}
