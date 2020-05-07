e51c797486c599ffb5711384a742881d1681bbef
Redouanelh <redouan_school@outlook.com>
2020-05-07 09:54:29 +0200

    Kleine aanpassingen in lambda

--- practicum2/LambdaRunner.cs
+++ practicum2/LambdaRunner.cs
@@ -20,15 +20,22 @@ namespace practicum2
             output.AppendFormat("addNumbers({0}, {1}, {2}) = {3}\n", num1, num2, num3, addNumbers(num1, num2, num3));
 
             // methode IsEven herschreven als lambda-expressie
-            Predicate<int> isEven = Methods.IsEven;
+            // Predicate<int> isEven = Methods.IsEven;
+
+            Predicate<int> isEven = x => x % 2 == 0;
             output.AppendFormat("isEven({0}) = {1}\n", num1, isEven(num1));
 
             // methode Num2String herschreven als lambda-expressie
-            Func<int, String> num2String = x => Methods.Num2String(x);
+            // Func<int, String> num2String = x => Methods.Num2String(x);
+            
+            Func<int, string> num2String = x => (x < 10 && x >= 0) ? new string[] 
+            { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }[x] : "undefined";
             output.AppendFormat("Num2String({0}) = {1}\n", num1, num2String(num1));
 
             // methode InBetween herschreven als lambda-expressie 
-            Func<int, int, int, bool> inBetween = (x, y, z) => Methods.InBetween(x, y, z);
+            // Func<int, int, int, bool> inBetween = (x, y, z) => Methods.InBetween(x, y, z);
+
+            Func<int, int, int, bool> inBetween = (x, y, z) => (x < y && y < z) || (z < y && y < x);
             output.AppendFormat("inBetween({0}, {1}, {2}) = {3}\n", num1, num2, num3, inBetween(num1, num2, num3));
             // (Ik wou hier eigenlijk een predicate, aangezien er een bool wordt gereturned, maar 't lukte me niet om meerdere parameters mee te geven aan een predicate)
 
